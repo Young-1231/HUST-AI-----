1886d48216eea7f845e727666b7f6879b3a51cdf
Young-1231 <HuangFeiyang1231@163.com>
2022-10-30 22:04:04 +0800

    add StepLR

---
+++ youngnet/nn/lr_scheduler.py
@@ -0,0 +1,31 @@
+from math import cos, pi
+import numpy as np
+from .optim import Optimizer
+
+
+class Scheduler:
+    def __init__(self, optimizer: Optimizer):
+        self.optimizer = optimizer
+        self.initial_lr = self.optimizer.lr
+        self.step_count = 0
+
+    def step(self):
+        self.step_count += 1
+        lr = self.get_lr()
+        self.optimizer.lr = lr
+
+    # 待重载的学习率调整方法
+    def get_lr(self):
+        raise NotImplementedError
+
+
+class StepLR(Scheduler):
+    def __init__(self, optimizer: Optimizer, step_size: int, gamma=0.1):
+        self.step_size = step_size
+        self.gamma = gamma
+        super().__init__(optimizer)
+
+    def get_lr(self):
+        if self.step_count % self.step_size == 0:
+            self.optimizer.lr *= self.gamma
+        return self.optimizer.lr
